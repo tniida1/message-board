51e7d5859b0cacefb1f32ef80995e5af1ff3ebca
tniida1 <tniida1@gmail.com>
2015-09-07 20:39:41 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,23 @@
 class MessagesController < ApplicationController
   def index
+    #Messageを全て取得する。
+    @messages = Message.all
   end
+
+  ## ここから追記
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to root_path , notice: 'メッセージを保存しました'
+  end
+  
+  # ここから下はPrivateメソッド
+  private
+  def message_params
+    # params[:message]のパラメータで、name , bodyのみを許可する
+    # 有効なものを明示して他をエラー=ホワイトリスト方式=ストロングパラメータ
+    # 返り値は ex:) {name:"入力されたname" , body: "入力されたbody" }
+    params.require(:message).permit(:name, :body)
+  end
+  ## ここまで
 end
